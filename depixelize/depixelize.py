bf67654a74a16a4ec0030023caf2f0c8ba7b994f
Isaias Suarez <ijsuarez@wustl.edu>
2016-12-08 23:08:23 -0600

    cleaned up code, added some comments, added flag parsing

--- depixelize/depixelize.py
+++ depixelize/depixelize.py
@@ -1,8 +1,8 @@
-import sys, math, png
+import argparse, math
+import png
 import networkx as nx
 import matplotlib.pyplot as plt
 import svgwrite
-import bspline
 
 from pyhull import qconvex
 
@@ -43,53 +43,57 @@ class Depixelizer:
     self.pixelData = {}
     self.pixelMapping = {}
     self.VEmap = {}
+    self.controlPoints = {}
     self.width = 0
     self.height = 0
     
   '''
   Main function to be called.
   '''
-  def depixelize(self, target):
+  def depixelize(self, args):
+    self.args = args
+    target = args.filename[0]
+
     graph = self.initializeGraph(target)
     voronoi = self.reshapeGraph(graph)
     visibleEdges = self.generateVisibleEdges(voronoi)
     self.resolveTJunctions(visibleEdges)
     
-    edgeGraph = nx.Graph()
-    edgeNum = 0
-    edgeColors = ['r', 'g', 'b']
-    for edge in visibleEdges:
-      for i in range(len(edge.pts)-1):
-        edgeGraph.add_node(edge[i], pos=(edge[i][0], self.height-edge[i][1]))
-        edgeGraph.add_edge(edge[i], edge[i+1], color=edgeColors[edgeNum % 3])
-      edgeGraph.add_node(edge[-1], pos=(edge[-1][0], self.height-edge[-1][1]))
-      #pos = nx.get_node_attributes(edgeGraph, 'pos')
-      #nx.draw(edgeGraph, pos, node_size=5)
-      #plt.savefig('visible%d.png'%edgeNum)
-      #plt.clf()
-      #edgeGraph.clear()
-      edgeNum+=1
+    if args.spline:
+      self.splineRender(visibleEdges)
+    elif args.line:
+      self.lineRender(visibleEdges)
+    else:
+      self.pixelRender()
 
-    #self.computeSplines(visibleEdges)
-    self.render(visibleEdges)
+    if args.debug:
+      pos = nx.get_node_attributes(graph, 'pos')
+      nx.draw(graph, pos, node_size=5)
+      plt.savefig('graph.png')
 
-    pos = nx.get_node_attributes(graph, 'pos')
-    nx.draw(graph, pos, node_size=5)
-    plt.savefig('graph.png')
+      plt.clf()
+      
+      pos = nx.get_node_attributes(voronoi, 'pos')
+      nx.draw(voronoi, pos, node_size=5)
+      plt.savefig('voronoi.png')
 
-    plt.clf()
-    
-    pos = nx.get_node_attributes(voronoi, 'pos')
-    nx.draw(voronoi, pos, node_size=5)
-    plt.savefig('voronoi.png')
+      plt.clf()
 
-    plt.clf()
+      edgeGraph = nx.Graph()
+      edgeColors = ['r', 'g', 'b']
+      edgeNum = 0
+      for edge in visibleEdges:
+        for i in range(len(edge.pts)-1):
+          edgeGraph.add_node(edge[i], pos=(edge[i][0], self.height-edge[i][1]))
+          edgeGraph.add_edge(edge[i], edge[i+1], color=edgeColors[edgeNum % 3])
+        edgeGraph.add_node(edge[-1], pos=(edge[-1][0], self.height-edge[-1][1]))
+        edgeNum+=1
 
-    pos = nx.get_node_attributes(edgeGraph, 'pos')
-    edges = edgeGraph.edges()
-    colors = [edgeGraph[u][v]['color'] for u,v in edges]
-    nx.draw(edgeGraph, pos, node_size=5, edges=edges, edge_color=colors)
-    plt.savefig('visible.png')
+      pos = nx.get_node_attributes(edgeGraph, 'pos')
+      edges = edgeGraph.edges()
+      colors = [edgeGraph[u][v]['color'] for u,v in edges]
+      nx.draw(edgeGraph, pos, node_size=5, edges=edges, edge_color=colors)
+      plt.savefig('visible.png')
     
   '''
   Takes a PNG image and turns it into a graph with YUV data.
@@ -333,7 +337,7 @@ class Depixelizer:
       for x in range(self.width):     
         pixel = self.pixelData[(x,y)]
         self.findVoronoiPts(graph, pixel)
-        hull = [point.split() for point in qconvex('p', pixel.voronoiPts)[2:]]
+        hull = [point.split() for point in qconvex('Qc p', pixel.voronoiPts)[2:]]
         hull = [(float(coord[0]),float(coord[1])) for coord in hull]
         pixel.voronoiPts = sorted(hull, key=lambda p: (self.angle(p, (pixel.coord[0]+0.5, pixel.coord[1]+0.5))))
         self.addVoronoiPts(voronoi, pixel)
@@ -433,6 +437,10 @@ class Depixelizer:
     else:
       pixel.voronoiPts.append((center[0] + (horizSign * 0.5), center[1] + (vertSign * 0.5)))
 
+  '''
+  Takes a voronoi diagram and generates a set of visible edges. Visible edges are connected
+  subgraphs where all nodes are valence-2.
+  '''
   def generateVisibleEdges(self, voronoi):
     result = set()
     for node in voronoi.nodes():
@@ -471,6 +479,9 @@ class Depixelizer:
 
     return result
 
+  '''
+  Walks through a given node's valence-2 neighbors to return the list of nodes in the visible edge.
+  '''
   def computeVisibleEdge(self, voronoi, node, neighbor):
     visited = set()
     visited.add(node)
@@ -488,6 +499,10 @@ class Depixelizer:
             stack.append(neighbor)
     return result
 
+  '''
+  Helper function to compare the polygons that lie on the edge of the two given nodes.
+  The number of incident polygons should never be greater than 2.
+  '''
   def dissimilarPolygons(self, node1, node2):
     intersect = self.pixelMapping[node1] & self.pixelMapping[node2]
     if len(intersect) == 1:
@@ -498,6 +513,11 @@ class Depixelizer:
     else:
       print len(intersect)
 
+  '''
+  Resolves T junctions as described in the paper. T junctions are located at nodes that have
+  three visible edges connected to them. We also merge visible edges when two disjoint edges
+  exist at a node.
+  '''
   def resolveTJunctions(self, visibleEdges):
     for point in self.VEmap:
       if len(self.VEmap[point]) == 2:
@@ -528,12 +548,19 @@ class Depixelizer:
           elif a2 > a0 and a2 > a1:
             self.mergeEdges(visibleEdges, point, ve2, ve0)
 
+  '''
+  Alternative helper function to compute the the angle between three points
+  using law of cosines.
+  '''
   def threePointAngle(self, p0, p1, p2):
     p01 = (p0[0] - p1[0])**2 + (p0[1] - p1[1])**2
     p02 = (p0[0] - p2[0])**2 + (p0[1] - p2[1])**2
     p12 = (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
     return math.degrees(math.acos((p01 + p02 -p12) / (2 * math.sqrt(p01 * p02))))
 
+  '''
+  Helper function to merge two given visible edges at a point.
+  '''
   def mergeEdges(self, visibleEdges, point, v1, v2):
     edge1, edge2 = v1.pts, v2.pts
     if edge1[0] != point: edge1.reverse()
@@ -562,6 +589,9 @@ class Depixelizer:
 
     visibleEdges.add(mergeEdge)
 
+  '''
+  Given two nodes, determines if the edge is a contour edge (not a shading edge).
+  '''
   def isContourEdge(self, node1, node2):
     intersect = self.pixelMapping[node1] & self.pixelMapping[node2]
     if len(intersect) == 1:
@@ -570,59 +600,219 @@ class Depixelizer:
       pixel1, pixel2 = intersect
       return not self.isShadingEdge(pixel1, pixel2)
 
+  '''
+  Given two nodes, determines if the edge is a shading edge.
+  '''
   def isShadingEdge(self, pixel1, pixel2):
     dist = (pixel1.y - pixel2.y)**2 + (pixel1.u - pixel2.u)**2 + (pixel1.v - pixel2.v)**2
     return dist <= (float(100/255)**2)
 
-  def computeSplines(self, visibleEdges):
-    for edge in visibleEdges:
-      spline = bspline.bspline(edge.pts, 3)
-
+  '''
+  Collapses collinear points on visible edge to preserve only the endpoints. In theory,
+  this collapses valence-2 nodes, but I might be misunderstanding what collapsing
+  valence-2 nodes means whoops.
+  '''
   def relaxVisibleEdge(self, visibleEdge):
     toRemove = set()
+    collinearPts = []
     if visibleEdge[0] == visibleEdge[-1]:
-      for i in range(len(visibleEdge.pts)):
-        prev = visibleEdge[i-1 % len(visibleEdge)]
+      edge = visibleEdge[:-1]
+      for i in range(len(edge)):
+        prev = visibleEdge[(i-1)%len(edge)]
         curr = visibleEdge[i]
-        nxt = visibleEdge[i+1 % len(visibleEdge)]
+        nxt = visibleEdge[(i+1)%len(edge)]
         if self.collinear(prev, curr, nxt):
           toRemove.add(curr)
+          if len(collinearPts) == 0:
+            collinearPts.append(prev)
+          collinearPts.append(curr)
+          #self.mergeControlPoints(prev, curr, nxt)
+        else:
+          if len(collinearPts) != 0:
+            collinearPts.append(prev)
+            #self.mergeControlPointsZwei((collinearPts[0], collinearPts[1]), (collinearPts[-2], collinearPts[-1]))
+            collinearPts = []
     else:
-      for i in range(1, len(visibleEdge.pts)-1):
+      for i in range(1, len(visibleEdge)-1):
         prev = visibleEdge[i-1]
         curr = visibleEdge[i]
         nxt = visibleEdge[i+1]
         if self.collinear(prev, curr, nxt):
           toRemove.add(curr)
-    return filter(lambda x: x not in toRemove, visibleEdge.pts)
+          if len(collinearPts) == 0:
+            collinearPts.append(prev)
+          collinearPts.append(curr)
+          #self.mergeControlPoints(prev, curr, nxt)
+        else:
+          if len(collinearPts) != 0:
+            collinearPts.append(prev)
+            #self.mergeControlPointsZwei((collinearPts[0], collinearPts[1]), (collinearPts[-2], collinearPts[-1]))
+            collinearPts = []
+    return filter(lambda x: x not in toRemove, visibleEdge)
       
+  '''
+  Helper function to determine if three given points are collinear. Can be removed
+  and code using this can be refactored to work with orient function instead.
+  '''
   def collinear(self, p0, p1, p2):
     x1, y1 = p1[0] - p0[0], p1[1] - p0[1]
     x2, y2 = p2[0] - p0[0], p2[1] - p0[1]
     return abs(x1 * y2 - x2 * y1) < 1e-12
 
-  def render(self, visibleEdges):
+  '''
+  Helper function to perform the orient test given three points.
+  '''
+  def orient(self, p0, p1, p2):
+    k = (p1[1] - p0[1]) * (p2[0] - p1[0]) - (p1[0] - p0[0]) * (p2[1] - p1[1])
+    if (k == 0):  #collinear
+      return 0
+    elif(k > 0):  #left
+      return 1
+    elif(k < 0):  #right
+      return -1
+
+  '''
+  Initializes the quadratic bspline control points of a visible edge to the pixel node
+  center of each adjacent point pair in the visible edge. Each point pair should be
+  initialized to the center node of the adjacent polygons.
+  '''
+  def initializeControlPoints(self, visibleEdge):
+    for i in range(len(visibleEdge)-1):
+      intersect = self.pixelMapping[visibleEdge[i]] & self.pixelMapping[visibleEdge[i+1]]
+      if visibleEdge[i] not in self.controlPoints:
+        self.controlPoints[visibleEdge[i]] = set()
+      if visibleEdge[i+1] not in self.controlPoints:
+        self.controlPoints[visibleEdge[i+1]] = set()
+      for node in intersect:
+        centerNode = Pixel(node.r, node.g, node.b, node.y, node.u, node.v, (node.coord[0]+0.5, node.coord[1]+0.5))
+        self.controlPoints[visibleEdge[i]].add(centerNode)
+        self.controlPoints[visibleEdge[i+1]].add(centerNode)
+
+  '''
+  Attempt to merge the quadratic bspline control points of edges on the visible edge when removing
+  collinear points. This does not actually work and probably won't work until we collapse valence-2
+  nodes properly before even making the visible edges. This will be deleted once the problem
+  is figured out.
+  '''
+  def mergeControlPoints(self, left, center, right):
+    leftControl = self.controlPoints[left] & self.controlPoints[center]
+    rightControl = self.controlPoints[center] & self.controlPoints[right]
+    if len(leftControl) > 2 or len(rightControl) > 2 or self.orient(left, center, right) != 0:
+      print 'problem', len(leftControl), len(rightControl)
+    toMerge = []
+    for l in leftControl:
+      for r in rightControl:
+        if self.orient(left, center, l.coord) == self.orient(center, right, r.coord):
+          toMerge.append((l, r))
+    for merge in toMerge:
+      leftCoord, rightCoord = merge[0].coord, merge[1].coord
+      mergeControl = Pixel(merge[0].r, merge[0].g, merge[0].b, merge[0].y, merge[0].u, merge[0].v,
+                          ((float(left[0]+right[0])/2, float(left[1]+right[1])/2)))
+      if merge[0] in self.controlPoints[left]:
+        self.controlPoints[left].remove(merge[0])
+      if merge[1] in self.controlPoints[right]:
+        self.controlPoints[right].remove(merge[1])
+      if merge[0] in self.controlPoints[center]:
+        self.controlPoints[center].remove(merge[0])
+      if merge[1] in self.controlPoints[center]: #hack
+        self.controlPoints[center].remove(merge[1])
+      self.controlPoints[left].add(mergeControl)
+      self.controlPoints[center].add(mergeControl)
+      self.controlPoints[right].add(mergeControl)
+
+  '''
+  Second attempt at merging control points. This also does not work and will also be deleted.
+  '''
+  def mergeControlPointsZwei(self, left, right):
+    leftControl = self.controlPoints[left[0]] & self.controlPoints[left[1]]
+    rightControl = self.controlPoints[right[0]] & self.controlPoints[right[1]]
+    toMerge = []
+    for l in leftControl:
+      for r in rightControl:
+        if self.orient(left[0], left[1], l.coord) == self.orient(left[0], left[1], r.coord):
+          toMerge.append((l, r))
+    mergeSet = set()
+    for merge in toMerge:
+      mergeControl = ((float(merge[0].coord[0]+merge[1].coord[0])/2, float(merge[0].coord[1]+merge[1].coord[1])/2))
+      mergeSet.add(mergeControl)
+    pixelSet = set()
+    for item in mergeSet:
+      pixel = Pixel(0,0,0,0,0,0,item)
+      pixelSet.add(pixel)
+    self.controlPoints[left[0]] = pixelSet
+    self.controlPoints[right[1]] = pixelSet
+
+  '''
+  Outputs the svg using just lines to represent the visible edges.
+  There is no attempt at colorization.
+  '''
+  def lineRender(self, visibleEdges):
     drawing = svgwrite.Drawing('output.svg')
     drawing.add(drawing.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(255,255,255)'))
-    path = []
     for visibleEdge in visibleEdges:
-      edge = self.relaxVisibleEdge(visibleEdge)
-      self.debugGraph(edge)
-      #path.append('M')
-      #path.append(self.scale(edge[0]))
-      color = set([p.rgbData() for p in self.pixelMapping[edge[0]]])
-      for i in range(0, len(edge)-3):
-        path.append('M')
+      edge = visibleEdge.pts
+      for i in range(1, len(edge)): # hack to resolve unknown duplication bug in visible edges
+        if edge[i] == edge[0]:
+          edge = edge[:i+1]
+          break
+      if (0,0) in edge: # hack to skip the border edges
+        continue
+      path = []
+      edge = self.relaxVisibleEdge(edge)
+      if self.args.debug:
+        self.debugGraph(edge)
+      path.append('M')
+      path.append(self.scale(edge[0]))
+      for i in range(len(edge)):
+        path.append('L')
         path.append(self.scale(edge[i]))
-        path.append('Q')
-        path.append(self.scale(edge[i+1]))
-        path.append(self.scale(edge[i+2]))
-        #color &= set([p.rgbData() for p in self.pixelMapping[edge[i]]]) & set([p.rgbData() for p in self.pixelMapping[edge[i+1]]])
-      #path.append('Z')
-    drawing.add(drawing.path(path, stroke=svgwrite.rgb(0,0,0), fill='none'))
+      drawing.add(drawing.path(path, stroke=svgwrite.rgb(0,0,0), fill='none'))
     drawing.save()
     plt.clf()
 
+  '''
+  Outputs the svg using quadratic bspline curves to represent the visible edges.
+  This sadly does not actually work.
+  '''
+  def splineRender(self, visibleEdges):
+    drawing = svgwrite.Drawing('output.svg')
+    drawing.add(drawing.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(255,255,255)'))
+    for visibleEdge in visibleEdges:
+      edge = visibleEdge.pts
+      for i in range(1, len(edge)): # hack to resolve unknown duplication bug in visible edges
+        if edge[i] == edge[0]:
+          edge = edge[:i+1]
+          break
+      if (0,0) in edge: # hack to skip the border edges
+        continue
+      path = []
+      self.initializeControlPoints(edge)
+      edge = self.relaxVisibleEdge(edge)
+      if self.args.debug:
+        self.debugGraph(edge)
+      for i in range(len(edge)-1):
+        controlPts = self.controlPoints[edge[i]] & self.controlPoints[edge[i+1]]
+        if len(controlPts) == 0: # if this bug occurs, make the control the midpoint
+          #print 'Edge does not have a control point?'
+          control = Pixel(0,0,0,0,0,0,(((edge[i][0]+edge[i+1][0])/2, (edge[i][1]+edge[i+1][1])/2)))
+        for point in controlPts:
+          control = point
+          if self.orient(edge[i], edge[i+1], point.coord) == 1:
+            break # arbitrarily go with control on the left
+        path.append('M')
+        path.append(self.scale(edge[i]))
+        path.append('Q')
+        path.append(self.scale(control.coord))
+        path.append(self.scale(edge[i+1]))
+      r, g, b = control.rgbData()
+      drawing.add(drawing.path(path, stroke=svgwrite.rgb(0,0,0), fill=svgwrite.rgb(r, g, b)))
+    drawing.save()
+    plt.clf()
+
+  '''
+  Debug function to output the graph of relaxed visible edges. Can probably be refactored to
+  put in a better place.
+  '''
   def debugGraph(self, edge):
     edgeGraph = nx.Graph()
     for i in range(len(edge)-1):
@@ -630,20 +820,45 @@ class Depixelizer:
       edgeGraph.add_edge(edge[i], edge[i+1])
     edgeGraph.add_node(edge[-1], pos=(edge[-1][0], self.height-edge[-1][1]))
 
-    #plt.clf()
     pos = nx.get_node_attributes(edgeGraph, 'pos')
-    nx.draw(edgeGraph, pos, node_size=5)
+    nx.draw(edgeGraph, pos, node_size=5, with_labels=False)
     plt.savefig('edge.png')
 
+  '''
+  Helper function to scale up the coordinates to look better in the svg output.
+  '''
   def scale(self, point):
     return (int(point[0] * 100), int(point[1] * 100))
+
+  '''
+  Outputs the svg using the voronoi diagram cells for a colored output.
+  '''
+  def pixelRender(self):
+    drawing = svgwrite.Drawing('output.svg')
+    for coord in self.pixelData:
+      pixel = self.pixelData[coord]
+      path = []
+      path.append('M')
+      path.append(self.scale(pixel.voronoiPts[0]))
+      for i in range(1, len(pixel.voronoiPts)):
+        path.append('L')
+        path.append(self.scale(pixel.voronoiPts[i]))
+      path.append('Z')
+      r,g,b = pixel.rgbData()
+      drawing.add(drawing.path(path, stroke=svgwrite.rgb(r,g,b), fill=svgwrite.rgb(r,g,b)))
+    drawing.save()
 
 '''
 Main method.
 '''
 if __name__ == '__main__':
-  if len(sys.argv) != 2:
-    print 'Missing argument: \'python depixelize.py <target_image>\''
-    exit()
+  parser = argparse.ArgumentParser(description='Depixelize a given pixel art image.')
+  parser.add_argument('filename', nargs=1, help='png file to depixelize')
+  parser.add_argument('-d', '--debug', action='store_true', help='output debug images')
+  parser.add_argument('-l', '--line', action='store_true', help='output line')
+  parser.add_argument('-s', '--spline', action='store_true', help='output quadratic bspline')
+  parser.add_argument('-p', '--pixel', action='store_true', help='output voronoi pixels')
+  args = parser.parse_args()
+  
   depixelizer = Depixelizer()
-  depixelizer.depixelize(sys.argv[1])
+  depixelizer.depixelize(args)
